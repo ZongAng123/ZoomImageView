c87823e6728ef2674de49faf864325cb0a994e48
ZongAng123 <zongang15@yahoo.com>
2017-02-23 14:48:28 +0800

    Initial Commit

---
+++ 缩放图像视图/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  缩放图像视图
+//
+//  Created by 纵昂 on 2017/2/23.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
