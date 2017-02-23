9fe7ce2f7eabb433eb4fead862829c9add1b8251
ZongAng123 <zongang15@yahoo.com>
2017-02-23 14:54:31 +0800

    ZoomImageView
    ZoomImageView

--- 缩放图像视图/ViewController.m
+++ 缩放图像视图/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "ZoomImageView.h"
 @interface ViewController ()
 
 @end
@@ -17,6 +17,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *grArrays=@[@0,@1,@2];
+    NSArray *titelsArray=@[@"单击触发",@"双击触发",@"长按触发"];
+    
+    __block float tempMaxY=60;
+    
+    [grArrays enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        
+        UIImageView *headerImageView = [[UIImageView alloc] initWithFrame:CGRectMake((self.view.frame.size.width-80)/2, tempMaxY, 80, 80)];
+        
+        headerImageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"userImage%ld",idx+1]];
+        
+        [self.view addSubview:headerImageView];
+        
+        tempMaxY=CGRectGetMaxY(headerImageView.frame)+70;
+        
+        KGestureRecognizerType gRType=[grArrays[idx] intValue];
+        
+        [[ZoomImageView getZoomImageView]showZoomImageView:headerImageView addGRType:gRType];
+        
+        
+        
+        UILabel *markLable = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetMinX(headerImageView.frame),CGRectGetMaxY(headerImageView.frame)+5, CGRectGetWidth(headerImageView.frame),20)];
+        markLable.text = titelsArray[idx];
+        
+        markLable.font = [UIFont systemFontOfSize:14];
+        markLable.textAlignment = NSTextAlignmentCenter;
+        [self.view addSubview:markLable];
+        
+    }];
+
 }
 
 - (void)didReceiveMemoryWarning {
